0a99ea0116867d05c968ad83eaef7be483f161cc
Dalraj Bains <dbains38@my.bcit.ca>
2025-11-14 10:29:58 -0800

    auth fix

--- COMP4977AIApp/COMP4977AIAppApp.swift
+++ COMP4977AIApp/COMP4977AIAppApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct COMP4977AIAppApp: App {
+    @StateObject private var authService = AuthService()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authService)
         }
     }
 }
--- COMP4977AIApp/ContentView.swift
+++ COMP4977AIApp/ContentView.swift
@@ -1,13 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var authService: AuthService
     
     var body: some View {
-        
-        MainTabView()
+        if authService.isAuthenticated {
+            MainTabView()
+        } else {
+            LoginView()
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .environmentObject(AuthService())
 }
